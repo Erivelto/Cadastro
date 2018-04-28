100d95c7281a688cec2bd0acbdce1291b7151ce4
Erivelto <fabioesimoes@gmail.com>
2018-04-28 14:14:54 -0300

    desenvolvimento
    desenvolvimento

--- GerenciadorFC.Cadastro.Servicos/Startup.cs
+++ GerenciadorFC.Cadastro.Servicos/Startup.cs
@@ -1,21 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using GerenciadorFC.Cadastro.Servicos.JwtModels;
+﻿using GerenciadorFC.Cadastro.Servicos.JwtModels;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.Threading.Tasks;
 
 namespace GerenciadorFC.Cadastro.Servicos
 {
-    public class Startup
+	public class Startup
     {
         public Startup(IConfiguration configuration)
         {
