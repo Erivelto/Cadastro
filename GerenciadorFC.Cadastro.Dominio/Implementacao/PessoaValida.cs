556ebc09f0faf65882df7ce2ecfc74e2b4757b08
Erivelto <fabioesimoes@gmail.com>
2018-06-06 01:46:08 -0300

    cobrança vericação
    cobrança vericação

---
+++ GerenciadorFC.Cadastro.Dominio/Implementacao/PessoaValida.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciadorFC.Cadastro.Dominio.Implementacao
+{
+    public class PessoaValida
+    {
+		public string Action { get; set; }
+		public string Controller { get; set; }
+    }
+}
---
+++ GerenciadorFC.Cadastro.Servicos/Repositorio/PessoaValidaRepositorio.cs
@@ -0,0 +1,23 @@
+﻿using GerenciadorFC.Cadastro.Dominio.Implementacao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorFC.Cadastro.Servicos.Repositorio
+{
+    public class PessoaValidaRepositorio
+    {
+		Contexto ctx = new Contexto();
+		public PessoaValida Obter(int codigoRep)
+		{
+			var valida = new PessoaValida();
+			var representanteLegal = ctx.Set<RepresentanteLegal>().Where(p => p.Codigo == codigoRep).FirstOrDefault();
+			var pessoa = ctx.Set<Pessoa>().Where(p => p.Codigo == representanteLegal.CodigoPessoa).FirstOrDefault();
+			var termo = ctx.Set<PessoaTermoDeUso>().Where(p => p.CodigoPessoa == pessoa.Codigo).FirstOrDefault();
+			var pessoCob = ctx.Set<PessoaCobranca>().Where(p => p.Reference == pessoa.Documento).FirstOrDefault();
+
+			return valida;
+		}
+	}
+}
